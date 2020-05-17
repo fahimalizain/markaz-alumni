a6426bceb2b03141964af73fe6114d47893c39f3
Fahim <faztp12@gmail.com>
2020-05-17 07:15:21 +0530

    Disabled Login Temporarily

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { Routes, RouterModule } from "@angular/router";
 const routes: Routes = [
   { path: "", loadChildren: "./modules/home/home.module#HomeModule" },
   { path: "home", redirectTo: "" },
-  { path: "login", loadChildren: "./modules/login/login.module#LoginModule" },
-  { path: "profile", loadChildren: "./modules/profile/profile.module#ProfileModule" },
-  {
-    path: "register",
-    loadChildren: "./modules/register/register.module#RegisterModule"
-  },
+  // { path: "login", loadChildren: "./modules/login/login.module#LoginModule" },
+  // { path: "profile", loadChildren: "./modules/profile/profile.module#ProfileModule" },
+  // {
+  //   path: "register",
+  //   loadChildren: "./modules/register/register.module#RegisterModule"
+  // },
   { path: "legal", loadChildren: "./modules/legal/legal.module#LegalModule" }
 ];
 
